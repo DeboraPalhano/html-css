b5f07811d327c29638dd44e467af8073aa1985fd
Debora Palhano <103783091+DeboraPalhano@users.noreply.github.com>
2022-06-27 22:58:10 +0100

    criei o exercício 022

--- Exercicíos/ex21/python/ex001.py
+++
@@ -1,5 +0,0 @@
-nome = input(" Digite seu nome = ")
-idade = int(input("digite sua idade = "))
-altura = float(input("digite sua altura = "))
-
-print("Olá",nome,", sua idade ",idade,", sua altura é ",altura)
